e7d79a00539d36bf762985dc577a355844f6792a
Harlan Hugh <hhugh@thebrain.com>
2022-10-07 17:25:38 -0700

    Anti-alias

--- Calculator.cs
+++ Calculator.cs
@@ -1,6 +1,71 @@
-﻿namespace TheBrainChallenge1;
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Text;
 
-public class Calculator
-{
-    
+namespace TheBrainChallenge1;
+
+public class Calculator {
+	
+	private const double AspectAdjust = 1;
+	
+	public Calculator() {
+	}
+
+	public string Square(int size) {
+		StringBuilder line = new();
+		for(int n = 0; n < size; n++) {
+			line.Append('*');
+		}
+
+		StringBuilder output = new();
+		for(int n = 0; n < size; n++) {
+			output.Append(line + "\n");
+		}
+
+		return output.ToString();
+	}
+	
+	public string Circle(int size) {
+
+		double cx = (size) / 2.0;
+		double cy = (size) / 2.0;
+
+		cx++;
+		cy++;
+
+		double rad = size / 2.0;
+
+		StringBuilder output = new();
+		for(double y = 0; y < size + 2; y++) {
+			for(double x = 0; x < size + 2; x++) {
+				double dist = CalcDist(x, y, cx, cy);
+				if(dist <= rad + 1) {
+					double amount = rad + 1 - dist;
+					if(amount >= 1) {
+						output.Append("*");
+					} else if(amount >= 0.8) {
+						output.Append("x");
+					} else if(amount >= 0.6) {
+						output.Append("+");
+					} else if(amount >= 0.4) {
+						output.Append("-");
+					} else if(amount >= 0.2) {
+						output.Append("'");
+					} else {
+						output.Append(".");
+					}
+				} else {
+					output.Append(" ");
+				}
+			}
+			output.Append("\n");
+		}
+		return output.ToString();
+	}
+
+	private double CalcDist(double x, double y, double cx, double cy) {
+		double dx = x - cx;
+		double dy = y - cy;
+		//dx *= AspectAdjust;
+		return Math.Sqrt(dx * dx + dy * dy);
+	}
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using TheBrainChallenge1;
+
+Console.WriteLine("TheBrain Challenge #1");
+
+while(true) {
+	Console.WriteLine("======================");
+	Console.WriteLine("Enter a size (or zero to exit):");
+
+	string sizeString = Console.ReadLine();
+	int.TryParse(sizeString, out int size);
+
+	if(size <= 0) {
+		break;
+	}
+
+	Calculator calc = new Calculator();
+
+	Console.WriteLine("======================");
+	Console.WriteLine("SQUARE:");
+	string square = calc.Square(size);
+	Console.Write(square);
+	Console.WriteLine("======================");
+	Console.WriteLine("CIRCLE:");
+	string circle = calc.Circle(size);
+	Console.Write(circle);
+	Console.WriteLine("======================");
+	Console.WriteLine("\n\n");
+}
