4bfde31905cf25abdfb57cd5c98363a04539146c
Harlan Hugh <hhugh@thebrain.com>
2022-10-09 22:16:02 -0700

    Render image as characters, input for vertical multiplier

--- Calculator.cs
+++ Calculator.cs
@@ -1,18 +1,25 @@
-﻿using Microsoft.VisualBasic.FileIO;
+﻿using SkiaSharp;
+using System.ComponentModel;
 using System.Text;
 
 namespace TheBrainChallenge1;
 
 public class Calculator {
 	
-	private const double YSize = 2.65;
+	public double YSize { get; set; } = 2.0;
 
 	private const double EdgeWidth = 1;
 
 	private const string grayscaleChars = " .:-=+*#%@";
 	private const string grayscaleBlocks = " ░▒▓█";
+
+	private SKBitmap bitmap;
 	
-	public Calculator() {
+	public Calculator(string path) {
+		SKImage image;
+		using(FileStream fs = new FileStream(path, FileMode.Open)) {
+			bitmap = SKBitmap.Decode(fs);
+		}
 	}
 
 	public string Square(int size, bool useBlocks) {
@@ -44,9 +51,11 @@ public class Calculator {
 		double rad = size / 2.0;
 
 		StringBuilder output = new();
-		for(double y = 0; y < size + 5; y += YSize) {
+		for(double y = 0; y < size + 4; y += YSize) {
 			for(double x = 0; x < size + 2; x++) {
+
 				double dist = CalcDist(x, y, cx, cy);
+				double brightness = 0;
 				if(dist <= rad + 1) {
 
 					double distFromEdge = Math.Abs(dist - rad);
@@ -55,16 +64,57 @@ public class Calculator {
 					double fillAmount = Math.Min(rad + 1 - dist, 1);
 					double fill = fillAmount * fillDarkness / 10.0;
 					double edge = edgeAmount * edgeDarkness / 10.0;
-					
-					int index = (int)Math.Min((fill + edge) * grayscaleBlocks.Length, grayscaleBlocks.Length - 1);
-					output.Append(grays[index]);
-				} else {
-					output.Append(" ");
+
+					brightness = fill + edge;
 				}
+
+				int index = (int)Math.Min((brightness) * grays.Length, grays.Length - 1);
+				output.Append(grays[index]);
 			}
 			output.Append("\n");
 		}
 		return output.ToString();
+	}
+
+	public string Picture(int size, bool useBlocks) {
+		SKSizeI skSize = new SKSizeI(size, size);
+
+		using(SKBitmap back = bitmap.Resize(skSize, SKFilterQuality.High)) {
+
+			double cx = (size) / 2.0;
+			double cy = (size) / 2.0;
+
+			string grays = useBlocks ? grayscaleBlocks : grayscaleChars;
+
+			double maxBrightness = 0.1;
+			double totalBrightness = 0;
+			for(double y = 0; y < size; y += YSize) {
+				for(double x = 0; x < size; x++) {
+
+					SKColor color = back.GetPixel((int)x, (int)y);
+					double brightness = (color.Red / 255.0) * 0.2126 + (color.Green / 255.0) * 0.7152 + (color.Blue / 255.0) * 0.0722;
+					maxBrightness = Math.Max(brightness, maxBrightness);
+					totalBrightness += brightness;
+				}
+			}
+			double averageBrightness = totalBrightness / (size * size);
+			//maxBrightness = Math.Min(averageBrightness * 2, maxBrightness);
+
+			StringBuilder output = new();
+			for(double y = 0; y < size; y += YSize) {
+				for(double x = 0; x < size; x++) {
+
+					SKColor color = back.GetPixel((int)x, (int)y);
+					double brightness = (color.Red / 255.0) * 0.2126 + (color.Green / 255.0) * 0.7152 + (color.Blue / 255.0) * 0.0722;
+					brightness *= 1 / maxBrightness;
+
+					int index = (int)Math.Min((brightness) * grays.Length, grays.Length - 1);
+					output.Append(grays[index]);
+				}
+				output.Append("\n");
+			}
+			return output.ToString();
+		}
 	}
 
 	private double CalcDist(double x1, double y1, double x2, double y2) {
--- Program.cs
+++ Program.cs
@@ -6,32 +6,36 @@ Console.WriteLine("TheBrain Challenge #1");
 
 string defFill = "10";
 string defEdge = "0";
-string defSize = "50";
+string defSize = "100";
+string defVert = "2.0";
 string defOut = "C";
 
 while(true) {
 	Console.WriteLine("======================");
 	
-	Console.WriteLine($"Chars or blocks (C or B) - default is {defOut}");
+	Console.WriteLine($"(C)hararters, (B)locks or e(X)it? - default is {defOut}");
 	string outString = Console.ReadLine().ToUpper();
+	if(outString == "X") {
+		break;
+	}
 	if(outString == "C" || outString == "B") {
 		defOut = outString;
 	}
 	bool useBlocks = defOut == "B";
 	
-	Console.WriteLine($"Enter a size (or X to exit or enter for {defSize}):");
+	Console.WriteLine($"Enter a size (enter for {defSize}):");
 	string sizeString = Console.ReadLine();
 	if(sizeString == "") {
 		sizeString = defSize;
 	}
+	int.TryParse(sizeString, out int size);
 
-	if(!int.TryParse(sizeString, out int size)) {
-		break;
+	Console.WriteLine($"Vertical multiplier (enter for {defVert}):");
+	string vertString = Console.ReadLine();
+	if(vertString == "") {
+		vertString = defVert;
 	}
-	
-	if(size <= 0) {
-		break;
-	}
+	double.TryParse(vertString, out double vert);
 
 	Console.WriteLine($"Fill darkness (1 to 10 or enter for {defFill}):");
 	string fillString = Console.ReadLine();
@@ -47,19 +51,26 @@ while(true) {
 	}
 	int.TryParse(edgeString, out int edge);
 
-	Calculator calc = new Calculator();
+	Calculator calc = new Calculator("../../../Background.png");
+	calc.YSize = vert;
 
-	Console.WriteLine("======================");
-	Console.WriteLine("SQUARE:");
-	Console.WriteLine("");
-	string square = calc.Square(size, useBlocks);
-	Console.Write(square);
-	Console.WriteLine("");
+	//Console.WriteLine("======================");
+	//Console.WriteLine("SQUARE:");
+	//Console.WriteLine("");
+	//string square = calc.Square(size, useBlocks);
+	//Console.Write(square);
+	//Console.WriteLine("");
 	Console.WriteLine("======================");
 	Console.WriteLine("CIRCLE:");
 	Console.WriteLine("");
 	string circle = calc.Circle(size, fill, edge, useBlocks);
 	Console.Write(circle);
+	Console.WriteLine("");
+	Console.WriteLine("======================");
+	Console.WriteLine("PICTURE:");
+	Console.WriteLine("");
+	string picture = calc.Picture(size, useBlocks);
+	Console.Write(picture);
 	Console.WriteLine("");
 	Console.WriteLine("======================");
 	Console.WriteLine("\n\n");
